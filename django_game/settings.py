e2ce362def3aeea4d9450fbf442deb759c3e8058
yi ming <1686447137@qq.com>
2023-03-13 15:29:38 +0800

    添加require

--- django_game/settings.py
+++ django_game/settings.py
@@ -76,6 +76,16 @@ WSGI_APPLICATION = "django_game.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    },
+}
+USER_AGENTS_CACHE = 'default'
 
 DATABASES = {
     "default": {
